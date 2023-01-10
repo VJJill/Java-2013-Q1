52050545889a93587f57938cf4f497ebd30be62a
sindunandana <bsindunandana@gmail.com>
2023-01-10 12:48:49 -0600

    sindu made changes in add

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,19 @@
 public class HelloWorld {
-    public void add(int a, int b){
-        int c=a+b;
-        System.out.print("Hello"+c);
+    public static void add(int a, int b) {
+        int c;
+         c = a + b;
+        System.out.print("Hello" + c);
     }
-    public static void  ygdisplay(){
+    public static void ygdisplay() {
         System.out.println("Hello Team !");
     }
     public static void main(String[] args) {
-        int c=substract(3,2);
-
-
-        System.out.println("Hello");
-    }
-    public static int substract(int n1, int n2){
-        return n1-n2;
-
-
+        int c = substract(3, 2);
         HelloWorld.ygdisplay();
-
+        System.out.println("Hello");
+        HelloWorld.add(12,5);
+    }
+    public static int substract(int n1, int n2) {
+        return n1 - n2;
     }
 }
