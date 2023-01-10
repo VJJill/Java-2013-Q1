06e3800c0352fd58cbd38cecee744769940d712b
anushamadala <anusha.madala@gmail.com>
2023-01-10 13:06:29 -0500

    Anusha's changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
+    public void add(int a, int b){
+        int c=a+b;
+        System.out.print("Hello"+c);
+    }
     public static void main(String[] args) {
+        
         System.out.println("Hello");
     }
 }
