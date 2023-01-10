664c552a6184016ae6ca51a3af12e528714b7090
Yamuna2022 <yglearn2022@gmail.com>
2023-01-10 12:57:13 -0500

    HelloWorld Page Updated. Yamuna

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
+    public static void  ygdisplay(){
+        System.out.println("Hello Team !");
+    }
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        HelloWorld.ygdisplay();
+
     }
 }
