2a6adf20abbac9c4587b15c1cafb2aaf8524a7ab
ReshJavaDev <reshmag1208@gmail.com>
2023-01-10 12:03:31 -0600

    committed by Reshma  with Git

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int c=substract(3,2);
+
         System.out.println("Hello");
+    }
+    public static int substract(int n1, int n2){
+        return n1-n2;
+
     }
 }
